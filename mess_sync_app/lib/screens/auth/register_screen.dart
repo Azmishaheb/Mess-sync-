676996ad66af7b83c4e3196a6f636e6ff9a3ff7e
Ayon <fmayon2023@gmail.com>
2025-07-15 12:32:33 +0600

    added initial page routing

---
+++ mess_sync_app/lib/screens/auth/register_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register")),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text("Register Screen"),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text("Back to Login"),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
